5afc8fcc7743a327ccf81ed237f487836bad7da1
leticea <nascimento.leticeah@gmail.com>
2023-08-01 20:12:24 -0300

    feat(global.ts): inserting global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,20 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
-    :focus {
-      outline: 0;
-      box-shadow: 0 0 2px ${props => props.theme['blue-200']};
-    }
   }
-`
+
+  :focus {
+      outline: 0;
+      box-shadow: 0 0 2px ${(props) => props.theme["blue-200"]};
+  }
+
+  body {
+    background-color: ${(props) => props.theme["base-background"]};
+    color: ${(props) => props.theme["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Nunito, sans-serif;
+  }
+`;
