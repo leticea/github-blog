57a94085ab7f1f41c98f03e3b762cca421aaa6d1
leticea <nascimento.leticeah@gmail.com>
2023-08-08 12:49:12 -0300

    feat(index.html): inserting font

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
+        <GlobalStyles />
         <Router />
       </BrowserRouter>
     </ThemeProvider>
