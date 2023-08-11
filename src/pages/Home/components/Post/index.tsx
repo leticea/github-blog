d410f1e54adb3dbca4bcae2a67fabebeb7ebb1ef
leticea <nascimento.leticeah@gmail.com>
2023-08-11 16:38:25 -0300

    feat(Post): creating Post component

---
+++ src/pages/Home/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import { PostContainer } from "./styles";
+
+export function Post() {
+  return (
+    <PostContainer>
+      <div>
+        <strong>JavaScript data types and data structures</strong>
+        <span>Há 1 dia</span>
+      </div>
+      <p>
+        Is a scripting language that enables you to create dynamically updating
+        content, control multimedia, animate images, and pretty much everything
+        else...
+      </p>
+    </PostContainer>
+  );
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const PostsListContainer = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+  margin-bottom: 14rem;
+`;
