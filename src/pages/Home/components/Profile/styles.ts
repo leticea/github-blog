92d74c33e8ecdeac366ffe00f023d9dbbd218cb0
leticea <nascimento.leticeah@gmail.com>
2023-08-10 21:40:49 -0300

    feat(Profile): inserting profile details

--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -36,4 +36,24 @@ export const ProfileDetails = styled.div`
       line-height: 130%;
     }
   }
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+    margin-top: auto;
+
+    li {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      svg {
+        width: 1.125rem;
+        height: 1.125rem;
+        color: ${({ theme }) => theme.colors["base-label"]};
+      }
+    }
+  }
 `;
