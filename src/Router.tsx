2899b22612a031b8df02fc09280706c4d7fa0c03
leticea <nascimento.leticeah@gmail.com>
2023-08-11 17:12:35 -0300

    feat(Post): creating Post page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Post } from "./pages/Post";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
@@ -7,6 +8,7 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
       </Route>
     </Routes>
   );
