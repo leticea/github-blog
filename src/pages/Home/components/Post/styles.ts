d08d8c2ef8a695004fedaf286d5a5330841e0fab
leticea <nascimento.leticeah@gmail.com>
2023-08-11 16:54:43 -0300

    feat(Post): editing style

--- src/pages/Home/components/Post/styles.ts
+++ src/pages/Home/components/Post/styles.ts
@@ -12,4 +12,35 @@ export const PostContainer = styled.div`
   &:hover {
     border-color: ${({ theme }) => theme.colors["base-label"]};
   }
+
+  div {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 1.25rem;
+
+    strong {
+      flex: 1;
+      font-size: ${({ theme }) => theme.textSizes["title-title-m"]};
+      color: ${({ theme }) => theme.colors["base-title"]};
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+    }
+
+    span {
+      width: max-content;
+      font-size: ${({ theme }) => theme.textSizes["text-text-s"]};
+      color: ${({ theme }) => theme.colors["base-span"]};
+    }
+  }
+
+  p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 4;
+  }
 `;
