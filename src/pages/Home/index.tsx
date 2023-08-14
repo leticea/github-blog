788351b53fd9495a854fd4cb8946a8313d10b06b
leticea <nascimento.leticeah@gmail.com>
2023-08-14 11:03:59 -0300

    feat(Home): getting response from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,22 +8,33 @@ import { api } from "../../lib/axios";
 const username = import.meta.env.VITE_GITHUB_USERNAME;
 const repoName = import.meta.env.VITE_GITHUB_REPONAME;
 
-interface IPost {
+export interface IPost {
   title: string;
+  body: string;
+  created_at: string;
+  number: number;
+  html_url: string;
+  comments: number;
+  user: {
+    login: string;
+  };
 }
 
 export function Home() {
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getPosts = useCallback(
     async (query: string = "") => {
       try {
+        setIsLoading(true);
         const response = await api.get(
           `/search/issues?q=${query}%20repo:${username}/${repoName}`
         );
         console.log(response.data);
         setPosts(response.data.items);
       } finally {
+        setIsLoading(false);
       }
     },
     [posts]
@@ -38,12 +49,9 @@ export function Home() {
       <Profile />
       <SearchInput />
       <PostsListContainer>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => (
+          <Post key={post.number} post={post} />
+        ))}
       </PostsListContainer>
     </>
   );
