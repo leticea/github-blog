48e7ec6146d3d419ad1b151f25be213e179fb20b
leticea <nascimento.leticeah@gmail.com>
2023-08-14 10:01:50 -0300

    feat(Home): creating getPosts function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
+import { useCallback, useState } from "react";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { PostsListContainer } from "./styles";
 
+interface IPost {
+  title: string;
+}
+
 export function Home() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  const getPosts = useCallback(async () => {
+    try {
+    } finally {
+    }
+  }, [posts]);
+
   return (
     <>
       <Profile />
