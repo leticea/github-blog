6b357330dd4a6b00fb8d8b74cf4c2c139c2e6a3b
leticea <nascimento.leticeah@gmail.com>
2023-08-14 15:56:06 -0300

    feat(Profile): creating async function for getProfileData

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -3,8 +3,42 @@ import { ExternalLink } from "../../../../components/ExternalLink";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../../../lib/axios";
+
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+
+interface ProfileData {
+  login: string;
+  bio: string;
+  avatar_url: string;
+  html_url: string;
+  name: string;
+  company?: string;
+  followers: number;
+}
 
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>(
+    {} as ProfileData
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get(`/users/${username}`);
+
+      setProfileData(response.data);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [profileData]);
+
+  useEffect(() => {
+    getProfileData();
+  }, []);
+
   return (
     <ProfileContainer>
       <ProfilePicture src="https://github.com/leticea.png" />
